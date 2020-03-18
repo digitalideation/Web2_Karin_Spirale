215914dff5fbccf06787bd66e9a42c2d67797765
Karin Fischer <karin.fischer@stud.hslu.ch>
2020-03-18 18:55:44 +0100

    Update Sketch_Vol_Spirale_Karin.js

--- public/Sketch_Vol_Spirale_Karin.js
+++ public/Sketch_Vol_Spirale_Karin.js
@@ -66,8 +66,8 @@ function draw(){
 
     if(settings.id!=undefined){
         var volmic = mic.getLevel();
-        let amplitudZ = floor(map(volmic,0,1,0,400));
-        let amplitude = floor(map(amplitudZ,0,1,0,400));
+        //let amplitudZ = floor(map(volmic,0,1,0,400));
+        let amplitude = floor(map(volmic,0,1,0,400));
         var data={
             vol:amplitude,
             id:settings.id
