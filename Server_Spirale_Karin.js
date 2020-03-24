4678145ecbe4759f107b3dc5eb4f9a7fd8caef02
Karin Fischer <karin.fischer@stud.hslu.ch>
2020-03-24 18:58:21 +0100

    Update Server_Spirale_Karin.js

--- Server_Spirale_Karin.js
+++ Server_Spirale_Karin.js
@@ -150,8 +150,8 @@ function newConnection(socket){
         
 
         function waveMicMsg (data){
-            //console.log("mein Schluessel"+ data.id)
-            console.log(allClientsInfo[data.id]);
+            console.log("mein Schluessel"+ data.id)
+            //console.log(allClientsInfo[data.id]);
 
           
 
@@ -218,7 +218,7 @@ function scale (num, in_min, in_max, out_min, out_max) {
       
 
      
-    console.log(Maxamplitude);
+    //console.log(Maxamplitude);
 
       //console.log(yvalues);
     
