1fd0abfcc33eb03e29654648ff1469234680b8af
hzuellig <zuellig@internauta.ch>
2020-03-24 08:47:08 +0100

    commit before merge

--- Server_Spirale_Karin.js
+++ Server_Spirale_Karin.js
@@ -21,19 +21,12 @@ var totalClients=0;
 
 
 // variabeln von Sketch
-
+//var audioContext = new AudioContext();
 
 
 
 let xspacing = 10; // Distance between each horizontal location
-
-
-
-let theta = 0.0; // Start angle at 0
-
 let period = 600.0; // How many pixels before the wave repeats
-
-
 
 let yvalues =[]; // Using an array to store height values for the wave
 let dx = (Math.PI*2 / period) * xspacing;// Value for incrementing x
@@ -88,6 +81,7 @@ function newConnection(socket){
 
         function startMsg(data){
 
+
         if(!socketIds.includes(socket.id)) {
             socketIds.push(socket.id);
             totalW+=data.w;
@@ -109,12 +103,20 @@ function newConnection(socket){
                 offsetbeginX:totalW - data.w,
                 offsetendX:totalW,
                 xspacing:xspacing+(Math.random()* 20),
-                dx:dx + (Math.random()* 9),
-                r:5+(Math.random()* 20),
-                 //color:'#'+Math.floor(Math.random()*16777215).toString(16),
-                colr:Math.floor(Math.random()* 255),
+                //xspacing:xspacing,
+                //dx:dx + (Math.random()* 9),
+                dx:dx,
+                //r:5+(Math.random()* 20),
                 colg:Math.floor(Math.random()* 255),
-                colb:Math.floor((Math.random() * 90) + 155)
+                //colr:Math.floor(Math.random()* 100),
+                colb:Math.floor((Math.random() * 90) + 205),
+                //colb:Math.floor(Math.random() * 100),
+                theta:0
+
+
+
+
+
 
 
             }
@@ -146,7 +148,7 @@ function newConnection(socket){
 
         function waveMicMsg (data){
             //console.log("mein Schluessel"+ data.id)
-            //console.log(allClientsInfo[data.id]);
+           // console.log(allClientsInfo[data.id]);
             if(!isNaN(data.vol)){
 
                 allClientsInfo[data.id].amplitud.push(data.vol);
@@ -157,7 +159,7 @@ function newConnection(socket){
                 allClientsInfo[data.id].Maxamplitude = getMaxOfArray(allClientsInfo[data.id].amplitud);
  
                 if(allClientsInfo[data.id].Maxamplitude < allClientsInfo[data.id].oldMaxAmp){
-                    allClientsInfo[data.id].Maxamplitude=allClientsInfo[data.id].oldMaxAmp*0.9;
+                    allClientsInfo[data.id].Maxamplitude=allClientsInfo[data.id].oldMaxAmp*0.8;
                 }
  
                 allClientsInfo[data.id].oldMaxAmp = allClientsInfo[data.id].Maxamplitude;
@@ -166,7 +168,7 @@ function newConnection(socket){
             
 
         //io.socket.emit ("waveMic", max);
-        if(allClientsInfo[data.id].amplitud.length>18){
+        if(allClientsInfo[data.id].amplitud.length>19){
             allClientsInfo[data.id].amplitud.splice(0,1);
         
         }
@@ -187,25 +189,27 @@ function scale (num, in_min, in_max, out_min, out_max) {
             calcWave(n);
         }
         io.sockets.emit('update',allClientsInfo ); //msg geht an alle clients
-    }, 1000); // 1000 ms / 60 -> 16.6666  entspricht ca dem timing in p5.js das 60mal pro sekunde draw aufruft
-
+    }, 16); // 1000 ms / 60 -> 16.6666  entspricht ca dem timing in p5.js das 60mal pro sekunde draw aufruft
+        //1000,500 anstatt 16
 
 
     function calcWave(n) {
         // Increment theta (try different values for
         // 'angular velocity' here)
         //ursprünglich von sketch
-        theta += 0.01;
+        allClientsInfo[n].theta += 0.01;
       
         // For every x value, calculate a y value with sine function
-        let x = theta;
+        let x = allClientsInfo[n].theta;
         for (let i = 0; i < allClientsInfo[n].yvalues.length; i++) {
             allClientsInfo[n].yvalues[i] = Math.cos(x) * allClientsInfo[n].Maxamplitude;
+            //x += (dx *allClientsInfo[n].Maxamplitude);
             x += dx;
-      
+            
+
         }
  
-
+      
 
      
       //console.log(Maxamplitude);
