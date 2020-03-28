fbd34f60729ceb0426c980fceb1ba811c2ef4f32
Karin Fischer <karin.fischer@stud.hslu.ch>
2020-03-28 10:28:59 +0100

    Update Server_Spirale_Karin.js

--- Server_Spirale_Karin.js
+++ Server_Spirale_Karin.js
@@ -175,7 +175,7 @@ function newConnection(socket){
             
 
         //io.socket.emit ("waveMic", max);
-        if(allClientsInfo[data.id].amplitud.length>9){
+        if(allClientsInfo[data.id].amplitud.length>10){
             allClientsInfo[data.id].amplitud.splice(0,1);
         
         }
